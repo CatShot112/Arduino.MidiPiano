eb015efec89401a9a6c9d61badf2712ee379a221
CatShot112 <CatShot112@wp.pl>
2019-09-21 19:48:34 +0200

    Create PinDefinitions.h

---
+++ PinDefinitions.h
@@ -0,0 +1,18 @@
+#define N0 A5
+#define N1 A4
+#define N2 A3
+#define N3 A2
+#define N4 A1
+#define N5 A0
+
+#define R0 12
+#define R1 11
+#define R2 10
+#define R3 9
+#define R4 8
+#define R5 7
+#define R6 6
+#define R7 5
+#define R8 4
+#define R9 3
+#define R10 2
